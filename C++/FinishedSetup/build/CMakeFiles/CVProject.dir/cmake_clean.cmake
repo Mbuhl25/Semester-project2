748ba6755be5c71a9619da99a6a4ea0406f8ee4a
fhcc2004 <fhcc2004@gmail.com>
2026-04-07 09:54:36 +0200

    Working min2phase

---
+++ C++/FinishedSetup/build/CMakeFiles/CVProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CVProject.dir/source/CV2ForCPP.cpp.o"
+  "CMakeFiles/CVProject.dir/source/CV2ForCPP.cpp.o.d"
+  "CMakeFiles/CVProject.dir/source/Kociemba.cpp.o"
+  "CMakeFiles/CVProject.dir/source/Kociemba.cpp.o.d"
+  "CMakeFiles/CVProject.dir/source/main.cpp.o"
+  "CMakeFiles/CVProject.dir/source/main.cpp.o.d"
+  "CVProject"
+  "CVProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CVProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
