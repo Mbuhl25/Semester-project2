c8cbf9e65f416b2c8be0a5d85326de314b7f7f96
fhcc2004 <fhcc2004@gmail.com>
2026-04-07 10:40:36 +0200

    CMakefiles back in buissness

---
+++ build/CMakeFiles/CVProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CVProject.dir/src/Color_Detection.cpp.o"
+  "CMakeFiles/CVProject.dir/src/Color_Detection.cpp.o.d"
+  "CMakeFiles/CVProject.dir/src/CppCubeSolver.cpp.o"
+  "CMakeFiles/CVProject.dir/src/CppCubeSolver.cpp.o.d"
+  "CVProject"
+  "CVProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CVProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/external/min2phaseCXX/CMakeFiles/min2phase.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/frederik-crone/Desktop/Semester-project2/external/min2phaseCXX/src/CubieCube.cpp" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/CubieCube.cpp.o" "gcc" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/CubieCube.cpp.o.d"
+  "/home/frederik-crone/Desktop/Semester-project2/external/min2phaseCXX/src/Search.cpp" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/Search.cpp.o" "gcc" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/Search.cpp.o.d"
+  "/home/frederik-crone/Desktop/Semester-project2/external/min2phaseCXX/src/coords.cpp" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/coords.cpp.o" "gcc" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/coords.cpp.o.d"
+  "/home/frederik-crone/Desktop/Semester-project2/external/min2phaseCXX/src/http.cpp" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/http.cpp.o" "gcc" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/http.cpp.o.d"
+  "/home/frederik-crone/Desktop/Semester-project2/external/min2phaseCXX/src/info.cpp" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/info.cpp.o" "gcc" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/info.cpp.o.d"
+  "/home/frederik-crone/Desktop/Semester-project2/external/min2phaseCXX/src/min2phase.cpp" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/min2phase.cpp.o" "gcc" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/min2phase.cpp.o.d"
+  "/home/frederik-crone/Desktop/Semester-project2/external/min2phaseCXX/src/tests.cpp" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/tests.cpp.o" "gcc" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/tests.cpp.o.d"
+  "/home/frederik-crone/Desktop/Semester-project2/external/min2phaseCXX/src/tools.cpp" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/tools.cpp.o" "gcc" "external/min2phaseCXX/CMakeFiles/min2phase.dir/src/tools.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
