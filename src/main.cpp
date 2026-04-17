e0587224ba3970e3e3cc177b63468d4e218873e1
Mathias Bergholt Buhl <mbbu@universal-robots.com>
2026-04-17 10:29:27 +0200

    Moved color_detection logic into main.cpp, Now we can use all our classes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
-#include "Algorithm.h"
 #include <vector>
 #include <string>
 #include <sstream>
+#include "Algorithm.h"
+#include "Color_Detection.h"
+
+#include <min2phase/min2phase.h>
+#include <min2phase/tools.h>
 
 int main() {
-
+    Color_Detection cube_mathi("Mathi", 1);
     Algorithm alg;
-
-    std::string input =
-        "R D R";
-
+    min2phase::init(); //precomputed coordinates
+    
+    std::string whole_cube = cube_mathi.rename_colors_to_orientations(cube_mathi.scan_whole_cube());
+    std::string input = min2phase::solve(whole_cube, 21, 1000000, 0, min2phase::APPEND_LENGTH | min2phase::USE_SEPARATOR, nullptr);
+    
     alg.moves = alg.FromStringToVec(input);
 
     std::string output = alg.switchpoints();
     std::cout << input << std::endl;
     std::cout << output << std::endl;
-
-    return 0;
 }
