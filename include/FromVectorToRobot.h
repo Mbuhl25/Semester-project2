7ff5e4a758b336d5ac14b298a550aaaa052be221
fhcc2004 <fhcc2004@gmail.com>
2026-04-28 13:05:04 +0200

    Ny klasse til at få roboten til at bevæge sig

---
+++ include/FromVectorToRobot.h
@@ -0,0 +1,18 @@
+#ifndef FROMVECTOROROBOT_H
+#define FROMVECTOROROBOT_H
+
+#include <vector>
+#include <string>
+#include "Algorithm.h"
+
+class FromVectorToRobot {
+
+public:
+    FromVectorToRobot() {}
+    std::string VecMove(std::vector<std::string> moves);
+
+private:
+
+};
+
+#endif
---
+++ src/FromVectorToRobot.cpp
@@ -0,0 +1,36 @@
+#include "FromVectorToRobot.h"
+#include "Algorithm.h"
+#include <iostream>
+#include <sstream>
+
+Algorithm alg;
+
+std::string FromVectorToRobot::VecMove(std::vector<std::string> moves){
+    alg.moves = moves;
+    std::string output = alg.switchpoints();
+
+    std::istringstream iss(output);
+    std::string move;
+    while (iss >> move) {
+        if      (move == "U")   { }
+        else if (move == "U'")  { }
+        else if (move == "2U")  { }
+        else if (move == "D")   { }
+        else if (move == "D'")  { }
+        else if (move == "2D")  { }
+        else if (move == "R")   { }
+        else if (move == "R'")  { }
+        else if (move == "2R")  { }
+        else if (move == "L")   { }
+        else if (move == "L'")  { }
+        else if (move == "2L")  { }
+        else if (move == "F")   { }
+        else if (move == "F'")  { }
+        else if (move == "2F")  { }
+        else if (move == "B")   { }
+        else if (move == "B'")  { }
+        else if (move == "2B")  { }
+    }
+
+    return output;
+}
