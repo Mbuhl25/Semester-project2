24a20ea16a0e07c6ff25ecb7dbba6254b7db6111
Mathias Bergholt Buhl <mbbu@universal-robots.com>
2026-04-29 22:37:22 +0200

    Made main.cpp into a template, of How we want it to run. All of the commented stuff is stuff to add, and then it should be able to solve the cube

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,86 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sstream>
-#include "Algorithm.h"
-#include "Color_Detection.h"
 
 #include <min2phase/min2phase.h>
 #include <min2phase/tools.h>
 
+#include "Algorithm.h"
+#include "Color_Detection.h"
+
 int main() {
     Algorithm algorithmProcessor;
-    // Color_Detection cube_object("Lucas", 3);
+    Color_Detection cube_object("Lucas", 1);
 
-    // // get the string for the positions of the cube, a solved cube has this string:
-    // // with colors: "YYYYYYYYYOOOOOOOOOGGGGGGGGGWWWWWWWWWRRRRRRRRRBBBBBBBBB"
-    // std::string cubeColorPlacements = cube_object.scan_whole_cube();
-    // // with orientations: "UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB"
-    // std::string cubeOrientationPlacements = cube_object.rename_colors_to_orientations(cubeColorPlacements);
+    // BEVÆG GRIPPEREN NED TIL TERNINGEN
 
-    // use min2phase to generate the solution for the rubix's cube
-    std::string cubeOrientationPlacements = "UUUUUUULURRRRRDRRRFFFBFFFBFDDDDDDDRDLLLULLLLLBBBFBFBBB";
-    min2phase::init();
-    std::string algorithmToSolve = min2phase::solve(cubeOrientationPlacements, 21, 1000000, 0, min2phase::APPEND_LENGTH | min2phase::USE_SEPARATOR, nullptr);
-    std::cout << algorithmToSolve << std::endl;
+    // GRIB FAST MED GRIPPEREN
 
-    // use the algorithm class to get the solution algorithm as a vector instead of a string
-    std::vector<std::string> algorithmToSolveVector = algorithmProcessor.FromStringToVec(algorithmToSolve);
-
-    std::string output = algorithmProcessor.switchpoints(algorithmToSolveVector);
+    // BEVÆG GRIPPEREN OP TIL KASSEN MED GUL SIDE ØVERST
     
-    std::cout << output << std::endl;
+    std::string sideY = cube_object.scan_one_side("Y");
 
-    // if      (move == "U")   { }
-    // else if (move == "U'")  { }
-    // else if (move == "2U")  { }
-    // else if (move == "D")   { }
-    // else if (move == "D'")  { }
-    // else if (move == "2D")  { }
-    // else if (move == "R")   { }
-    // else if (move == "R'")  { }
-    // else if (move == "2R")  { }
-    // else if (move == "L")   { }
-    // else if (move == "L'")  { }
-    // else if (move == "2L")  { }
-    // else if (move == "F")   { }
-    // else if (move == "F'")  { }
-    // else if (move == "2F")  { }
-    // else if (move == "B")   { }
-    // else if (move == "B'")  { }
-    // else if (move == "2B")  { }
+    // BEVÆG GRIPPEREN OP TIL KASSEN MED ORANGE SIDE ØVERST
+
+    std::string sideO = cube_object.scan_one_side("O");
+
+    // BEVÆG GRIPPEREN OP TIL KASSEN MED GRØN SIDE ØVERST
+
+    std::string sideG = cube_object.scan_one_side("G");
+
+    // BEVÆG GRIPPEREN OP TIL KASSEN MED HVID SIDE ØVERST
+
+    std::string sideW = cube_object.scan_one_side("W");
+
+    // BEVÆG GRIPPEREN OP TIL KASSEN MED RØD SIDE ØVERST
+
+    std::string sideR = cube_object.scan_one_side("R");
+
+    // BEVÆG GRIPPEREN OP TIL KASSEN MED BLÅ SIDE ØVERST
+
+    std::string sideB = cube_object.scan_one_side("B");
+
+    
+    std::string cubeColorPlacements = sideY + sideO + sideG + sideW + sideR + sideB;                            // get the string for the positions of the cube, a solved cube has this string: "YYYYYYYYYOOOOOOOOOGGGGGGGGGWWWWWWWWWRRRRRRRRRBBBBBBBBB"
+    
+    std::string cubeOrientationPlacements = cube_object.rename_colors_to_orientations(cubeColorPlacements);     // and expressed as an orientations: "UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB"
+
+    
+    min2phase::init();      // use min2phase to generate the solution for the rubix's cube
+    std::string algorithmToSolve = min2phase::solve(cubeOrientationPlacements, 21, 1000000, 0, min2phase::APPEND_LENGTH | min2phase::USE_SEPARATOR, nullptr);
+    std::cout << "This is the algorithm to solve the cube: \n" << algorithmToSolve << std::endl;
+
+    
+    std::vector<std::string> algorithmToSolveVector = algorithmProcessor.FromStringToVec(algorithmToSolve);     // use the algorithm class to get the solution algorithm as a vector instead of a string
+
+    std::string algorithmToSolveInBox = algorithmProcessor.switchpoints(algorithmToSolveVector);
+    std::cout << "This is the algorithm to solve the cube while it is sitting in the box: \n" << algorithmToSolveInBox << std::endl;
+
+
+
+    std::vector<std::string> algorithmToSolveInBoxVector = algorithmProcessor.FromStringToVec(algorithmToSolveInBox);
+    for (int i = 0; i < algorithmToSolveInBoxVector.size(); ++i) {
+        std::string move = algorithmToSolveInBoxVector[i];
+
+        std::cout << "now time to move: " << move << std::endl;
+
+        // if      (move == "U")   {robotMoveFunction_U()}
+        // else if (move == "U'")  {robotMoveFunction_Uprime()}
+        // else if (move == "U2")  {robotMoveFunction_U(); robotMoveFunction_U();}
+        // else if (move == "D")   {robotMoveFunction_D()}
+        // else if (move == "D'")  {robotMoveFunction_Dprime()}
+        // else if (move == "D2")  {robotMoveFunction_D(); robotMoveFunction_D()}
+        // else if (move == "R")   {robotMoveFunction_R()}
+        // else if (move == "R'")  {robotMoveFunction_Rprime()}
+        // else if (move == "R2")  {robotMoveFunction_R(); robotMoveFunction_R()}
+        // else if (move == "L")   {robotMoveFunction_L()}
+        // else if (move == "L'")  {robotMoveFunction_Lprime()}
+        // else if (move == "L2")  {robotMoveFunction_L(); robotMoveFunction_L()}
+        // else if (move == "F")   {robotMoveFunction_F()}
+        // else if (move == "F'")  {robotMoveFunction_Fprime()}
+        // else if (move == "F2")  {robotMoveFunction_F(); robotMoveFunction_F()}
+        // else if (move == "B")   {robotMoveFunction_B()}
+        // else if (move == "B'")  {robotMoveFunction_Bprime()}
+        // else if (move == "B2")  {robotMoveFunction_B(); robotMoveFunction_B()}
+    }
 }
