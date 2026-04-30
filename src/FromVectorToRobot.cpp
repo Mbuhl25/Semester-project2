7df7a6366df43e056570ce5acf2224af64953058
OnkelGunnGunn <ebl022005@gmail.com>
2026-04-30 15:51:39 +0200

    testing moves

--- src/FromVectorToRobot.cpp
+++ src/FromVectorToRobot.cpp
@@ -9,21 +9,27 @@
 
 //std::string FromVectorToRobot::VecMove(std::vector<std::string> moves){}
 
-std::vector<double> working_frame = {0.107044, -0.381108, 0.1755723, -2.14, -1.57, 0};
+std::vector<double> grip_point = {0.22657, -0.285007, 0.0422215, 0.620214, -3.07384, 0};
 
 void move_to_work_start(){
 
 ur_rtde::RTDEControlInterface rtde_control("192.168.1.11");
 
-rtde_control.moveJ({1.95257, -1.69021, 2.28666, -2.14271, -1.57025, 0}, 0.5, 0.3);
+rtde_control.moveL({0.210568, -0.283833, 0.130525, 0.552248, -3.07929, 0}, 0.5, 0.3);
 
 }
 
+void move_grip_point(){
+    ur_rtde::RTDEControlInterface rtde_control("192.168.1.11");
+
+    rtde_control.moveL(grip_point, 0.5, 0.3);
+}
 
 void FromVectorToRobot::MoveU(){
     ur_rtde::RTDEControlInterface rtde_control("192.168.1.11");
 
-rtde_control.moveL(working_frame, 0.5, 0.3);
+    grip_point();
+
 }
 
 void FromVectorToRobot::MoveUPrime(){
@@ -97,6 +103,5 @@ void FromVectorToRobot::MoveB2(){
 int main(){
     FromVectorToRobot move;
     
-    move.MoveU();
     move_to_work_start();
 }
