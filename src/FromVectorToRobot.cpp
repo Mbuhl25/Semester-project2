c81ff16b9f4b1bdaf7f17e0bb27efa0fcf8a1691
OnkelGunnGunn <ebl022005@gmail.com>
2026-04-30 15:15:44 +0200

    testing moves

--- src/FromVectorToRobot.cpp
+++ src/FromVectorToRobot.cpp
@@ -1,16 +1,29 @@
 #include "FromVectorToRobot.h"
 #include <ur_rtde/rtde_control_interface.h>
 #include <ur_rtde/rtde_receive_interface.h>
-#include "Algorithm.h"
+//#include "Algorithm.h"
 #include <iostream>
 #include <sstream>
 
-Algorithm alg;
+//Algorithm alg;
 
-std::string FromVectorToRobot::VecMove(std::vector<std::string> moves){}
+//std::string FromVectorToRobot::VecMove(std::vector<std::string> moves){}
+
+std::vector<double> working_frame = {0.107044, -0.381108, 0.1755723, -2.14, -1.57, 0};
+
+void move_to_work_start(){
+
+ur_rtde::RTDEControlInterface rtde_control("192.168.1.11");
+
+rtde_control.moveJ({1.95257, -1.69021, 2.28666, -2.14271, -1.57025, 0}, 0.5, 0.3);
+
+}
+
 
 void FromVectorToRobot::MoveU(){
+    ur_rtde::RTDEControlInterface rtde_control("192.168.1.11");
 
+rtde_control.moveL(working_frame, 0.5, 0.3);
 }
 
 void FromVectorToRobot::MoveUPrime(){
@@ -79,4 +92,11 @@ void FromVectorToRobot::MoveBPrime(){
 
 void FromVectorToRobot::MoveB2(){
 
+}
+
+int main(){
+    FromVectorToRobot move;
+    
+    move.MoveU();
+    move_to_work_start();
 }
