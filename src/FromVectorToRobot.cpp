917e6b75d8e628573134aa7f926bbc3c19fdfcad
fhcc2004 <fhcc2004@gmail.com>
2026-04-30 14:34:57 +0200

    Begin

--- src/FromVectorToRobot.cpp
+++ src/FromVectorToRobot.cpp
@@ -7,32 +7,30 @@
 
 Algorithm alg;
 
-std::string FromVectorToRobot::VecMove(std::vector<std::string> moves){
-    alg.moves = moves;
-    std::string output = alg.switchpoints();
+std::string FromVectorToRobot::VecMove(std::vector<std::string> moves){}
 
-    std::istringstream iss(output);
-    std::string move;
-    while (iss >> move) {
-        if      (move == "U")   { }
-        else if (move == "U'")  { }
-        else if (move == "2U")  { }
-        else if (move == "D")   { }
-        else if (move == "D'")  { }
-        else if (move == "2D")  { }
-        else if (move == "R")   { }
-        else if (move == "R'")  { }
-        else if (move == "2R")  { }
-        else if (move == "L")   { }
-        else if (move == "L'")  { }
-        else if (move == "2L")  { }
-        else if (move == "F")   { }
-        else if (move == "F'")  { }
-        else if (move == "2F")  { }
-        else if (move == "B")   { }
-        else if (move == "B'")  { }
-        else if (move == "2B")  { }
-    }
-
-    return output;
+void FromVectorToRobot::MoveU(){
 }
+void FromVectorToRobot::MoveUPrime(){
+
+}
+void FromVectorToRobot::MoveD(){
+    
+}
+        // else if (move == "U'")  {robotMoveFunction_Uprime()}
+        // else if (move == "U2")  {robotMoveFunction_U(); robotMoveFunction_U();}
+        // else if (move == "D")   {robotMoveFunction_D()}
+        // else if (move == "D'")  {robotMoveFunction_Dprime()}
+        // else if (move == "D2")  {robotMoveFunction_D(); robotMoveFunction_D()}
+        // else if (move == "R")   {robotMoveFunction_R()}
+        // else if (move == "R'")  {robotMoveFunction_Rprime()}
+        // else if (move == "R2")  {robotMoveFunction_R(); robotMoveFunction_R()}
+        // else if (move == "L")   {robotMoveFunction_L()}
+        // else if (move == "L'")  {robotMoveFunction_Lprime()}
+        // else if (move == "L2")  {robotMoveFunction_L(); robotMoveFunction_L()}
+        // else if (move == "F")   {robotMoveFunction_F()}
+        // else if (move == "F'")  {robotMoveFunction_Fprime()}
+        // else if (move == "F2")  {robotMoveFunction_F(); robotMoveFunction_F()}
+        // else if (move == "B")   {robotMoveFunction_B()}
+        // else if (move == "B'")  {robotMoveFunction_Bprime()}
+        // else if (move == "B2")  {robotMoveFunction_B(); robotMoveFunction_B()}
