298871164dc75c906a298dfb71874578a04171e5
fhcc2004 <fhcc2004@gmail.com>
2026-04-30 14:50:03 +0200

    All

--- include/FromVectorToRobot.h
+++ include/FromVectorToRobot.h
@@ -11,6 +11,42 @@ public:
     FromVectorToRobot() {}
     std::string VecMove(std::vector<std::string> moves);
 
+    void MoveU();
+
+    void MoveUPrime();
+
+    void MoveU2();
+
+    void MoveD();
+
+    void MoveDPrime();
+
+    void MoveD2();
+
+    void MoveR();
+
+    void MoveRPrime();
+
+    void MoveR2();
+
+    void MoveL();
+
+    void MoveLPrime();
+
+    void MoveL2();
+
+    void MoveF();
+
+    void MoveFPrime();
+
+    void MoveF2();
+
+    void MoveB();
+
+    void MoveBPrime();
+
+    void MoveB2();
+
 private:
 
 };
