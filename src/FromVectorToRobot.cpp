f94d60bc25c224bacfdbc1028cdc76c40f9ffeac
fhcc2004 <fhcc2004@gmail.com>
2026-04-30 14:46:57 +0200

    All moves

--- src/FromVectorToRobot.cpp
+++ src/FromVectorToRobot.cpp
@@ -10,27 +10,73 @@ Algorithm alg;
 std::string FromVectorToRobot::VecMove(std::vector<std::string> moves){}
 
 void FromVectorToRobot::MoveU(){
+
 }
+
 void FromVectorToRobot::MoveUPrime(){
 
 }
-void FromVectorToRobot::MoveD(){
-    
+
+void FromVectorToRobot::MoveU2(){
+
 }
-        // else if (move == "U'")  {robotMoveFunction_Uprime()}
-        // else if (move == "U2")  {robotMoveFunction_U(); robotMoveFunction_U();}
-        // else if (move == "D")   {robotMoveFunction_D()}
-        // else if (move == "D'")  {robotMoveFunction_Dprime()}
-        // else if (move == "D2")  {robotMoveFunction_D(); robotMoveFunction_D()}
-        // else if (move == "R")   {robotMoveFunction_R()}
-        // else if (move == "R'")  {robotMoveFunction_Rprime()}
-        // else if (move == "R2")  {robotMoveFunction_R(); robotMoveFunction_R()}
-        // else if (move == "L")   {robotMoveFunction_L()}
-        // else if (move == "L'")  {robotMoveFunction_Lprime()}
-        // else if (move == "L2")  {robotMoveFunction_L(); robotMoveFunction_L()}
-        // else if (move == "F")   {robotMoveFunction_F()}
-        // else if (move == "F'")  {robotMoveFunction_Fprime()}
-        // else if (move == "F2")  {robotMoveFunction_F(); robotMoveFunction_F()}
-        // else if (move == "B")   {robotMoveFunction_B()}
-        // else if (move == "B'")  {robotMoveFunction_Bprime()}
-        // else if (move == "B2")  {robotMoveFunction_B(); robotMoveFunction_B()}
+
+void FromVectorToRobot::MoveD(){
+
+}
+
+void FromVectorToRobot::MoveDPrime(){
+
+}
+
+void FromVectorToRobot::MoveD2(){
+
+}
+
+void FromVectorToRobot::MoveR(){
+
+}
+
+void FromVectorToRobot::MoveRPrime(){
+
+}
+
+void FromVectorToRobot::MoveR2(){
+
+}
+
+void FromVectorToRobot::MoveL(){
+
+}
+
+void FromVectorToRobot::MoveLPrime(){
+
+}
+
+void FromVectorToRobot::MoveL2(){
+
+}
+
+void FromVectorToRobot::MoveF(){
+
+}
+
+void FromVectorToRobot::MoveFPrime(){
+
+}
+
+void FromVectorToRobot::MoveF2(){
+
+}
+
+void FromVectorToRobot::MoveB(){
+
+}
+
+void FromVectorToRobot::MoveBPrime(){
+
+}
+
+void FromVectorToRobot::MoveB2(){
+
+}
